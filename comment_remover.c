03faac81994e96ebee8d665202b0862c560a4dc8
RenshinH <68468634+RenshinH@users.noreply.github.com>
2020-07-23 13:29:23 +0300

    Create comment_remover.c

---
+++ comment_remover.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define MAX 1000
+
+char line[MAX];
+
+void read()
+{
+    char c;
+    int i = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        line[i] = c;
+        i++;
+    }
+}
+
+void print()
+{
+    int i;
+    int y = 0;
+
+    for (i = 0; line[i] != '\0'; i++)
+    {
+        if (line[i] == '\\' && line[i+1] == '\\')
+        {
+            while (line[i] != '\n')
+            {
+                i++;
+            }
+
+            i++;
+        }
+        printf("%c", line[i]);
+    }
+}
+
+int main()
+{
+
+    read();
+    print();
+
+    return 0;
+}
